a9b73421273373164a70508645937a5030d8c8d8
M. Rafly Saputra <966raflisaputra@gmail.com>
2025-06-06 20:30:08 +0700

    membuat page project

--- project_page.py
+++ project_page.py
@@ -1,14 +1,71 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+from component.awan import Awan
 
 class Project:
     def __init__(self, window):
         self.frame_project = tk.Frame(window, width=1100, height=650)
         self.frame_project.place(x=0, y=0)
         
-        self.back = tk.Button(self.frame_project, text="Back", font=("pixelify sans", 20), command=self.to_menu)
-        self.back.place(x=50, y=50)
         
-        #Teks
-        self.welcome = tk.Label(self.frame_project, text="PROJEK BROO", font=("04b", 80))
-        self.welcome.place(x=0, y=150)
+        #Next
+        self.next = tk.Button(self.frame_project, text="Next", font=("pixelify sans", 20), bg="#6f6f6f", fg="white", command=self.to_welcome)
+        self.next.place(x=877, y=580, width=193, height=50)
+
+        #Prev
+        self.prev = tk.Button(self.frame_project, text="Prev", font=("pixelify sans", 20), bg="#6f6f6f", fg="white", command=self.to_menu)
+        self.prev.place(x=30, y=580, width=193, height=50)      
+        
+        #frame header
+        self.header = tk.Label(self.frame_project, border=0)
+        self.header.place(relwidth=.9, y=50, x=60)
+        
+        self.profile = tk.Frame(self.header, bd=2, width="150", height="150", relief="solid")
+        self.profile.pack(side="left")
+        
+        self.h1 = tk.Label(self.header, border=0, text="Rafsanss", font=("04b", 40), fg="#000", highlightthickness=0)
+        self.h1.pack(side="left", padx=20)
+        
+        self.h2 = tk.Label(self.frame_project, text="Our Project:", font=("04b", 20), fg="#000", highlightthickness=0)
+        self.h2.place(x=55, y=240)
+        
+        self.project = tk.Label(self.frame_project)
+        self.project.place(x=35, y=290, relwidth=.9)
+        
+        
+        for i in range(4):
+            self.box = tk.Frame(self.project, bd=2, relief="solid", width="200", height="200", background="#f6f6f6")
+            self.box.grid(column=i, row=0, padx=25)
+            
+            self.judul = tk.Label(self.project, text=f"Project {i + 1}", fg="#000000", font=("04b", 15))
+            self.judul.grid(column=i, row=1, pady=10)
+            
+            
+        # KOMPONEN
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=15, y=120)
+        
+        # titit
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=230, y=30)
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=430, y=45)
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=630, y=60)
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=1050, y=30)
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=770, y=180)
+        self.titit = tk.Frame(self.frame_project, width=10, height=10, bg="#FCF998")
+        self.titit.place(x=900, y=100)
+        
+        # matahari
+        self.titit = tk.Frame(self.frame_project, width=100, height=80, bg="#FCF998")
+        self.titit.place(x=910, y=120)
+        
+        # awan
+        Awan(tk=tk, frame=self.frame_project, bgcolor="#d9d9d9", horizontal=970, vertical=155, width=110, height=30)
+        Awan(tk=tk, frame=self.frame_project, bgcolor="#d9d9d9", horizontal=950, vertical=180, width=147, height=38)
+        Awan(tk=tk, frame=self.frame_project, bgcolor="#d9d9d9", horizontal=910, vertical=210, width=300, height=25)
+        
+        
