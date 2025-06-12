1bccf21c12088012f767d5a8417a69b9fea6fac3
zhraraa <annisa.azzahra1207@gmail.com>
2025-06-12 14:54:15 +0700

    Update welkam, tambah easter egg

--- welcome_page.py
+++ welcome_page.py
@@ -75,16 +75,24 @@ class Welcome:
         self.bird2.place(x=1050, y=203, width=38, height=16)
         
         #Awan 3 bang
-        Awan(tk=tk, frame=self.frame_welcome, bgcolor="#d9d9d9", horizontal=1000, vertical=320, width=123.12, height=38.7)
-        Awan(tk=tk, frame=self.frame_welcome, bgcolor="#d9d9d9", horizontal=950, vertical=355, width=197, height=44.22)
+        Awan(tk=tk, frame=self.frame_welcome, bgcolor="#A5A2A2", horizontal=1000, vertical=320, width=123.12, height=38.7)
+        Awan(tk=tk, frame=self.frame_welcome, bgcolor="#A5A2A2", horizontal=950, vertical=355, width=197, height=44.22)
         
-    
         #Easter Egg
-        self.tes = tk.Button(self.frame_welcome, text="", bg="#d9d9d9", command=self.easter_egg, borderwidth=0, highlightthickness=0)
-        self.tes.place(x=115, y=250, width=91, height=25)
+        self.getCodee = tk.Button(self.frame_welcome, text="", bg="#d9d9d9", command=self.easter_egg, borderwidth=0, highlightthickness=0)
+        self.getCodee.place(x=115, y=250, width=91, height=25)
+        
+        self.buttonCode = tk.Button(self.frame_welcome, text="", bg="#d9d9d9", command=self.getCodew, borderwidth=0, highlightthickness=0)
+        self.buttonCode.place(x=88, y=274, width=149, height=38)
+        
+        self.buttonCode1 = tk.Button(self.frame_welcome, text="", bg="#d9d9d9", command=self.getCodeww, borderwidth=0, highlightthickness=0)
+        self.buttonCode1.place(x=905, y=170, width=123, height=28)
+        
+        self.buttonCode2 = tk.Button(self.frame_welcome, text="", bg="#A5A2A2", command=self.getCodewww, borderwidth=0, highlightthickness=0)
+        self.buttonCode2.place(x=950, y=355, width=197, height=44.22)
         
     def easter_egg(self):
-        pop_up = tk.Toplevel(bg="blue")
+        pop_up = tk.Toplevel(bg="pink")
         pop_up.resizable(False, False)
         window_width = 550
         window_height = 325
@@ -96,27 +104,42 @@ class Welcome:
         pop_up.geometry(f"{window_width}x{window_height}+{x}+{y}")
         
         list_code = {
-            "Bunga" : "Kucing Myauuu",
-            "Buah" : 0
+            "chweyi"    : "one",
+            "crabywan"  : "two",
+            "yippiee"    : "three",
         }
         
         code = tk.Entry(pop_up)
         code.pack(pady=50)
         
         meme1 = tk.PhotoImage(file="image/meme1.png")
-        meme1_label = tk.Label(pop_up, image=meme1, borderwidth=0, highlightthickness=0)
+        meme1_label = tk.Label(self.frame_welcome, image=meme1, borderwidth=0, highlightthickness=0)
         meme1_label.image = meme1
         
-        teks = tk.Label(pop_up, text="", bg="blue")
+        meme2 = tk.PhotoImage(file="image/meme2.png")
+        meme2_label = tk.Label(self.frame_welcome, image=meme2, borderwidth=0, highlightthickness=0)
+        meme2_label.image = meme2
+        
+        meme3 = tk.PhotoImage(file="image/meme3.png")
+        meme3_label = tk.Label(self.frame_welcome, image=meme3, borderwidth=0, highlightthickness=0)
+        meme3_label.image = meme3
+        
+        teks = tk.Label(pop_up, text="", bg="pink")
         teks.pack()
         
         def redeem_code():
             code_redeem = code.get()
             
             try:
-                if list_code[code_redeem] == "Kucing Myauuu":
-                    meme1_label.place(x=0, y=0, width=meme1.width(), height=meme1.height())
-                    teks.config(text=list_code[code_redeem])
+                if list_code[code_redeem] == "one":
+                    pop_up.destroy()
+                    meme1_label.place(x=966, y=10, width=meme1.width(), height=meme1.height())
+                if list_code[code_redeem] == "two":
+                    pop_up.destroy()
+                    meme2_label.place(x=80, y=430, width=meme2.width(), height=meme2.height())
+                if list_code[code_redeem] == "three":
+                    pop_up.destroy()
+                    meme3_label.place(x=890, y=453, width=meme3.width(), height=meme3.height())
             except KeyError:
                 teks.config(text="KODE REDEEM TIDAK VALID")
                     
@@ -125,6 +148,15 @@ class Welcome:
         
         redeem = tk.Button(pop_up, text="Redeem", command=redeem_code)
         redeem.pack()
+        
+    def getCodew(self):
+        messagebox.showinfo("yeyy", "Redeem Code : crabywan")
+    
+    def getCodeww(self):
+        messagebox.showinfo("for u too", "Redeem Code : yippiee")
+        
+    def getCodewww(self):
+        messagebox.showinfo("for u", "Redeem Code : chweyi")
         
     def about(self):
         self.frame_welcome.place_forget()
@@ -147,7 +179,7 @@ class Welcome:
         self.bubble1_label = tk.Label(self.frame_about, image=self.bubble1, borderwidth=0, highlightthickness=0)
         self.bubble1_label.place(x=285, y=429, width=self.bubble1.width(), height=self.bubble1.height())
         
-        about_text = ["MAU TAU BERBAGAI GAME SERU\nDALAM PROGRAM INI?", "Hmmmmmm??", "MULAI DARI UAP INVADER, GAME PESAWAT YANG\nMENANTANG UNTUK MENEMBAK MUSUH-MUSUHNYA,\nFEEDING FRENZY, PERMAINAN SERU TENTANG\nIKAN YANG SALING MEMANGSA,", "Hmmmmmm Hmmm Hmmmm", "RASAKAN PENGALAMAN BERMAIN YANG\nMENYENANGKAN DAN MENANTANG\nDISETIAP GAMENYA,CUMA DI PROJEK\nBLOCKBUSTER UwU AJA LOCHH", "Hmmmmmmm Hmmmmmm!!!"]
+        about_text = ["MAU TAU BERBAGAI GAME SERU\nDALAM PROGRAM INI?", "Hmmmmmm??\n(Translate: Apaan tuuh??)", "MULAI DARI UAP INVADER, GAME PESAWAT YANG\nMENANTANG UNTUK MENEMBAK MUSUH-MUSUHNYA,\nFEEDING FRENZY, PERMAINAN SERU TENTANG\nIKAN YANG SALING MEMANGSA,", "Hmmmmmm Hmmm...\n(Translate: Eh eh tapii....)", "RASAKAN PENGALAMAN BERMAIN YANG\nMENYENANGKAN DAN MENANTANG\nDISETIAP GAMENYA,CUMA DI PROJEK\nBLOCKBUSTER UwU AJA LOCHH", "Hmmmmmmm Hmmmmm Hmmm\n(Translate: ada kejutam lain juga lohh)", "Hmmm? Hmmm hmmm\n(Translate: Mau tau? kadang awan\npunya banyak rahasia tauu...)"]
         
         self.text1 = tk.Label(self.frame_about, text=about_text.pop(0), font=("pixelify sans", 20), bg="#fefefe", anchor="center")
         self.text1.place(x=325, y=255, width=460, height=105)
